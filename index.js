eefd3f5e22264fe1e692ab77aa383627ed07308b
Asibur Rahman <asibur70@gmail.com>
2025-06-26 16:36:46 +0600

    Udpdate data

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ async function run() {
 
 
     app.get("/recentTasks", async (req, res) => {
-      const result = await taskCollection.find().sort({ date: 1 }).limit(6).toArray();
+      const result = await taskCollection.find().sort({ date: 1 }).limit(8).toArray();
       res.send(result);
     })
 
